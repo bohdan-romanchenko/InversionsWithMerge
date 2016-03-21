a1989605945a30c10d5efe85c05011ba42863627
Romanchenko Bohdan <nadman3@gmail.com>
2016-03-21 19:23:50 +0200

    optimized algorithm. now it's n^2*log(n)

--- src/is41_Romanchenko_02.java
+++ src/is41_Romanchenko_02.java
@@ -1,135 +1,120 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 /**
- * Created by nadman on 19.03.16.
+ * Created by Bohdan Romanchenko on 1/2/2016.
  */
 public class is41_Romanchenko_02 {
+
     private static int[][] inputtedArray;
-    private static int countUsers;
-    private static int countFilms;
+    private static int countOfUsers;
+    private static int countOfFilms;
     private static int aimUser;
-    private static int[] countOfInvertions;
+    private static int[] countInvertions;
 
-    public static void main(String[] args) {
-
-        Scanner read = null;
+    public static void main(String[] args) throws IOException {
         try {
-            read = new Scanner(new File(args[0]));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            Scanner read = new Scanner(new File(args[0]));
 
-        assert read != null;
-        countUsers = read.nextInt();
-        countFilms = read.nextInt();
-        aimUser = Integer.parseInt(args[1]);
-        inputtedArray = readIntArrayFromFile(read, countUsers, countFilms);
-        inputtedArray = convertInArray();
-        amountOfInvertions();
-        output();
-    }
+            countOfUsers = read.nextInt();
+            countOfFilms = read.nextInt();
 
-    private static void output(){
-        int[] answers;
+            aimUser = Integer.parseInt(args[1]);
 
-        answers = countOfInvertions.clone();
-        Arrays.sort(countOfInvertions);
-        String answerString = "";
-        answerString += aimUser + "\n";
-        for (int i = 1; i < countUsers; i++)
-            for (int j = 0; j < countUsers; j++)
-                if (answers[j] == countOfInvertions[i]){
-                    answers[j] = Integer.MAX_VALUE;
-                    answerString += ((j + 1) + " " + countOfInvertions[i] + "\n");
-                    break;
-                }
-        File outputFile = new File("is41_Romanchenko_02_output.txt");
-        try {
-            FileWriter fw = new FileWriter(outputFile);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(answerString);
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            countInvertions = new int[countOfUsers];
+
+            inputtedArray = readFromFile(read);
+            amountOfInvertions();
+            output();
+        }catch (FileNotFoundException e){
+            System.err.println("Wrong input file");
         }
     }
 
-    private static void amountOfInvertions() {
-        countOfInvertions = new int[countUsers];
-        for (int i = 0; i < countUsers; i++)
-            mergeSort(inputtedArray[i], 1, countFilms, i);
-    }
-
-    private static int[][] readIntArrayFromFile(Scanner read, int rows, int columns){
+    public static int[][] readFromFile(Scanner read){
         int returnArray[][];
 
-        returnArray = new int[rows][columns + 1];
+        returnArray = new int[countOfUsers][countOfFilms];
 
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < columns + 1; j++)
+        for (int i = 0; i < countOfUsers; i++){
+            read.nextInt();
+            for (int j = 0; j < countOfFilms; j++)
                 returnArray[i][j] = read.nextInt();
+        }
         return returnArray;
     }
 
-    //n^3. !! ISSUE !! wrong is here ;(
-    private static int[][] convertInArray(){
-        int [][] returnArray = new int[countUsers][countFilms + 1];
-        for (int i = 0; i < countUsers; i++){
-            System.arraycopy(inputtedArray[i], 0, returnArray[i], 0, countFilms + 1);
-        }
-        int [] aimArray = new int[countFilms + 1];
-        aimArray[0] = aimUser;
-        for (int i = 0; i < countUsers; i++){
-            for (int j = 1; j < countFilms + 1; j++){
-                returnArray[i][inputtedArray[i][j]] = j;
-                aimArray[j] = returnArray[aimUser - 1][j];
+    //n*n*log(n)
+    public static void amountOfInvertions(){
+        int[] comparedUsersArray = new int[countOfFilms];
+        for (int i = 0; i < countOfUsers; i++) {
+            for (int j = 0; j < countOfFilms; j++){
+                comparedUsersArray[ inputtedArray[aimUser - 1][j] - 1 ] = inputtedArray[i][j] - 1;
             }
+            countInvertions[i] = mergeSort(comparedUsersArray, 0, comparedUsersArray.length - 1);
         }
-
-        for (int i = 0; i < countUsers; i ++)
-            for (int j = 1; j < countFilms + 1; j++)
-                for (int key = 1; key < countFilms + 1; key++)
-                    if (returnArray[i][j] == aimArray[key]) {
-                        returnArray[i][j] = key;
-                        break;
-                    }
-        return returnArray;
     }
 
-    private static void merge(int []A, int p, int q, int r, int numberArray){
-        int n1 = q - p + 1, n2 = r - q;
-        int LeftArray[] = new int[n1 + 1], RightArray[] = new int[n2 + 1];
+    public static void output() {
+        LinkedList<int[]> tempInversionsMatrix = new LinkedList<>();
+        for (int i = 0; i < countInvertions.length; i++)
+            tempInversionsMatrix.add(new int[]{i, countInvertions[i]});
+        Arrays.sort(countInvertions);
+        String answerToWrite = "";
+        answerToWrite += aimUser + "\n";
+        for (int i = 0; i < countOfUsers; i++)
+            for (int j = 0; j < tempInversionsMatrix.size(); j++) {
+                if (countInvertions[i] == tempInversionsMatrix.get(j)[1] && countInvertions[i] != 0) {
+                    answerToWrite += ((tempInversionsMatrix.get(j)[0]+1)  + " " + countInvertions[i]) + "\n";
+                    tempInversionsMatrix.remove(j);
+                    break;
+                }
+            }
+        try{
+            PrintWriter writer = new PrintWriter(new File("is41_Romanchenko_02_output.txt"), "UTF-8");
+            writer.println(answerToWrite);
+            writer.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("problems with output file");
+        }
+    }
 
-        System.arraycopy(A, p, LeftArray, 0, n1);
 
+    private static int merge(int[] A, int firstPoint, int middlePoint, int lastPoint){
+        int n1 = middlePoint - firstPoint + 1, n2 = lastPoint - middlePoint;
+        int L[] = new int[n1 + 1], R[] = new int[n2 + 1];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = A[firstPoint + i];
         for (int i = 0; i < n2; i++)
-            RightArray[i] = A[q + i + 1];
+            R[i] = A[middlePoint + i + 1];
 
-        LeftArray[n1] = Integer.MAX_VALUE;
-        RightArray[n2] = Integer.MAX_VALUE;
+        L[n1] = Integer.MAX_VALUE;
+        R[n2] = Integer.MAX_VALUE;
         int i = 0, j = 0;
 
-        for(int k = p; k <= r; k++){
-            if(LeftArray[i] <= RightArray[j]){
-                A[k] = LeftArray[i];
+        int counter = 0;
+        for(int k = firstPoint; k <= lastPoint; k++){
+            if(L[i] <= R[j]){
+                A[k] = L[i];
                 i++;
             }
             else {
-                A[k] = RightArray[j];
+                A[k] = R[j];
                 j++;
-                countOfInvertions[numberArray] += (n1 - i);
+                counter += n1 - i;
             }
         }
+        return counter;
     }
 
-    public static void mergeSort(int[] A, int left, int right, int numberArray){
-        if (left < right) {
-            int middle = (left + right) / 2;
-            mergeSort(A, left, middle, numberArray);
-            mergeSort(A, middle + 1, right, numberArray);
-            merge(A, left, middle, right, numberArray);
-        }
+    public static int mergeSort(int[] A, int leftPoint, int rightPoint){
+        if(leftPoint >= rightPoint) return 0;
+        int middlePoint = (leftPoint + rightPoint) / 2;
+        return mergeSort(A, leftPoint, middlePoint) + mergeSort(A, middlePoint + 1, rightPoint) + merge(A, leftPoint, middlePoint, rightPoint);
     }
 }
